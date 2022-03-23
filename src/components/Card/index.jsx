f6d122fbd9b45e9c373403f6e20fcf2880adcf72
Talgat <tairovtalgat@gmail.com>
2022-03-23 12:43:27 +0600

    init

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,18 @@
+
+
+export const Card = () => {
+    return (
+        <div>
+            <img src="" alt=""/>
+            <div>
+                Название
+            </div>
+            <div>
+                Цена
+            </div>
+            <div>
+                Размер
+            </div>
+        </div>
+    );
+};
