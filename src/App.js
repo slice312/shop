1d3801533e27510165f30b9f0d037e33efc3c6db
Talgat <tairovtalgat@gmail.com>
2022-03-23 15:32:18 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {MDBContainer, MDBRow, MDBCol} from "mdb-react-ui-kit";
-import {Header} from "./components/Header";
+import {Header} from "./components/Header/Header";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Header/>
+
+            <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="" className="d-block w-100" alt="mo"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="" className="d-block w-100" alt="das."/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src="" className="d-block w-100" alt="gfdg"/>
+                    </div>
+                </div>
+            </div>
             <Routes>
                 {/*<Route path="/" element={<Home/>}/>*/}
                 {/*    <Route path="/register" element={<Register/>}/>*/}
