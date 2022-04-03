734261b8005131049edbfe5266c4bc35b68dfb40
Talgat <tairovtalgat@gmail.com>
2022-04-03 23:49:13 +0600

    футер

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,35 @@
+import {Link} from "react-router-dom";
 import css from "./styles.module.scss";
+import developersLogo from "src/assets/icons/developers-logo.svg";
+
 
 export const Footer = () => {
-   return (
-       <div className={css.root}>
-           футер
-       </div>
-   ) ;
+    return (
+        <div className={css.root}>
+            <div className={css.container}>
+                <div className={css.blocks}>
+                    <div className={css.logo}>
+                        <img src={developersLogo} alt={developersLogo}/>
+                    </div>
+                    <div className={css.links}>
+                        <p>Компания</p>
+                        <Link to="/about">О нас</Link>
+                        <Link to="/news">Новости</Link>
+                        <Link to="/help">Помощь</Link>
+                    </div>
+                    <div className={css.contacts}>
+
+                    </div>
+                    <div className={css.social}>
+
+                    </div>
+                </div>
+                <div className={css.createdBy}>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
 };
